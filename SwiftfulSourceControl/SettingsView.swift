5eec51605e9afbf9bbba32922ee907d514698f84
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-08 10:42:38 +0100

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Thomas on 1/8/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}

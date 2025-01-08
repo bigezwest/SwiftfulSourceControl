8b0fd10b4d4ffce15ab7e18e30ba61f1bac87271
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-08 05:14:14 +0100

    [Feature] Fix text

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
                                 .font(.largeTitle)
                                 .foregroundStyle(.tint)
                             Text("Some New Title")
-                            Text("Swiftful Thinking !!!")
+                            Text("Swiftful Thinking!")
                             
                             Button("Click Me!") {
                             }

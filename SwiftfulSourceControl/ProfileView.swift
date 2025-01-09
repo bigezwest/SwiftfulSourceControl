0682d1afcaf621913915bd3c8fa92f6daf11b79e
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-09 05:31:21 +0100

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Thomas on 1/9/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
